a22abfc8b0d91ba6ff0adaa208477752daeaa450
M4tt0tito <francisca.marin01@alumnos.ucn.cl>
2026-09-14 01:24:45 -0300

    Ordenar carpetas + vista + png

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -8,7 +8,7 @@ extends CharacterBody2D
 
 var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-@onready var sprite: ColorRect = $ColorRect
+@onready var sprite: Sprite2D = $Sprite2D
 
 
 func _physics_process(delta: float) -> void:
@@ -17,14 +17,14 @@ func _physics_process(delta: float) -> void:
 		velocity.y += gravity * delta
 
 	# Salto (usa la acción integrada de Godot "ui_accept" -> Espacio / Enter)
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if (Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("ui_up") or Input.is_key_pressed(KEY_W)) and is_on_floor():
 		velocity.y = jump_velocity
 
 	# Movimiento horizontal (usa acciones integradas "ui_left" / "ui_right" -> flechas)
 	var direction: float = Input.get_axis("ui_left", "ui_right")
 	if direction != 0.0:
 		velocity.x = direction * speed
-		sprite.scale.x = -1.0 if direction < 0.0 else 1.0
+		sprite.flip_h = direction < 0.0
 	else:
 		velocity.x = move_toward(velocity.x, 0.0, speed)
 
